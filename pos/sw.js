f974eca4d5a357ed1d309b07255cfd4c2c643a66
Echarpe <mahmoud.morsy5577@gmail.com>
2026-07-29 14:19:40 -0300

    Add files via upload

--- pos/sw.js
+++ pos/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'store-apps-shell-v204';
+const CACHE_NAME = 'store-apps-shell-v208';
 
 // ⚠️ مفيش skipWaiting تلقائي.
 // النسخة الجديدة بتنزل في الخلفية وتستنى، والصفحة هي اللي بتقرر
