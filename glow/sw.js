f21a6fdc5278d07df6a6e29452a32beada5a8567
Echarpe <mahmoud.morsy5577@gmail.com>
2026-08-17 17:33:55 -0300

    Add files via upload

--- glow/sw.js
+++ glow/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glow-loyalty-v65';
+const CACHE_NAME = 'glow-loyalty-v66';
 
 // ============ استقبال إشعارات Push (حتى والتطبيق مقفول) ============
 self.addEventListener('push', (event) => {
