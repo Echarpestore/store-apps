e9cf8a3aa0338199b3a7afdb81f77f520935eb67
Echarpe <mahmoud.morsy5577@gmail.com>
2026-08-18 20:07:38 -0300

    Add files via upload

--- loyalty/sw.js
+++ loyalty/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'echarpe-loyalty-v76';
+const CACHE_NAME = 'echarpe-loyalty-v77';
 
 // ============ استقبال إشعارات Push (حتى والتطبيق مقفول) ============
 self.addEventListener('push', (event) => {
