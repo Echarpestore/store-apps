b704dcedb53129015508d92edcb0fb573a852b2f
Echarpe <mahmoud.morsy5577@gmail.com>
2026-09-11 18:31:04 +0300

    Add files via upload

--- Office/sw.js
+++ Office/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'echarpe-office-v665';
+const CACHE_NAME = 'echarpe-office-v666';
 self.addEventListener('install', (e)=> self.skipWaiting());
 self.addEventListener('activate', (e)=>{
   e.waitUntil(
